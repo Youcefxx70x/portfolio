48e776dd779736d4ce414b64171b7ce68b609306
Youcefxx70x <youcef.h.ouahab@gmail.com>
2025-09-29 05:35:29 +0100

    fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 
-import profile2 from "../assets/p.png"
+import profile2 from "../assets/p2.png"
 import {HERO_CONTENT} from "../constants/index"
 import { motion } from "framer-motion"
 import { var_ar,var_fr } from "../constants/index"
@@ -25,13 +25,13 @@ const Hero = ({lang}) => {
         visible:{opacity:1,x:0,transition:{duration:0.5}}
     }
   return (
-    <div className="pb-4 lg:mb-34">
-      <div className="flex flex-wrap lg:flex-row-reverse">
-        <div className="w-full lg:w-1/2">
-            <div className="flex justify-center sm:justify-end items-center h-full">
+    <div className="pt-4  lg:mb-34 mt-4">
+      <div className="flex flex-wrap lg:flex-row-reverse ">
+        <div className="w-full lg:w-2/4 lg:flex lg:justify-start g ">
+            <div className="flex justify-center sm:justify-center items-center h-full">
                <motion.img 
                 src={profile2}
-                className="w-40 h-60 sm:w-60 sm:h-80 lg:h-120  rounded-full md:w-72 lg:w-80 border border-stone-900 md:rounded-3xl "
+                className="w-40 h-60 sm:w-60 sm:h-80 lg:h-100  rounded-full md:w-72 lg:w-90 border border-stone-900 md:rounded-3xl shadow-lg shadow-stone-900 lg:ml-10"
                 initial={{ x: 100, opacity: 0 }}
                 animate={{ x: 0, opacity: 1 }}
                 transition={{ duration: 1, delay: 1.5 }}
@@ -39,10 +39,10 @@ const Hero = ({lang}) => {
 
             </div>
         </div>
-        <div className="w-full lg:w-1/2 ">
-                <motion.div className="flex flex-col items-center lg:items-start mt-10" initial="hidden" animate="visible" variants={containerVariants} >
+        <div className="w-full lg:w-2/4  ">
+                <motion.div className="flex flex-col items-center lg:items-end mt-10 lg:text-end " initial="hidden" animate="visible" variants={containerVariants} >
                     <motion.h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl" variants={childVariants}>{lang=="GB"?"Youcef Habib Ouahab":varSelector(lang).name}</motion.h2>
-                    <motion.span className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent" variants={childVariants}>{lang=="GB"?"Full-Stack Developer":varSelector(lang).title}</motion.span>
+                    <motion.span className={`bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent ${lang=="AR"? "self-end":"self-center"}`} variants={childVariants}>{lang=="GB"?"Full-Stack Developer":varSelector(lang).title}</motion.span>
                     <motion.p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter" variants={childVariants}>{lang=="GB"?HERO_CONTENT:varSelector(lang).HERO_CONTENT}</motion.p>
                 </motion.div>
 
